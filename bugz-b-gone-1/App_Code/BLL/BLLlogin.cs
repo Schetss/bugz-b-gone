fc781c433a7f540616a58fe2fb884c191dfe5d04
SchetsS <stijn.schets@telenet.be>
2014-07-14 21:03:43 +0200

    First insertions in database

---
+++ bugz-b-gone-1/App_Code/BLL/BLLlogin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for BLLlogin
+/// </summary>
+public class BLLlogin
+{
+	public BLLlogin()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
---
+++ bugz-b-gone-1/App_Code/DAL/DALlogin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for DALlogin
+/// </summary>
+public class DALlogin
+{
+	public DALlogin()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
